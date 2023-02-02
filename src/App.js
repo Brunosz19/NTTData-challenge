1dd9273c82f5a251a3f44eb5da9d1cb4251c0dae
brunosz19 <hesz.bruno05@gmail.com>
2023-02-02 07:28:23 -0500

    ADD: install React Table

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function App() {
+  const [data, setData] = useState()
   
+  useEffect(() => {
+    axios
+      .get("https://randomuser.me/api/?results=15")
+      .then((response) => setData(response.data.results))
+      .catch((error) => console.log(error))
+  }, []);
+
   return (
     <div>
-      <h1>HOLA</h1>
+      <div style={{ display: "flex", gap: "35px" }}>
+        <h1>Nombre</h1>
+        <h1>Apellido</h1>
+        <h1>Edad</h1>
+        <h1>Genero</h1>
+        <h1>Email</h1>
+        <h1>Nacionalidad</h1>
+        <h1>Foto</h1>
+      </div>
+      <div style={{ display: "flex", gap: "35px" }}>
+        <h1>{data ? data[0].name.first : null}</h1>
+        <h1>Apellido</h1>
+        <h1>Edad</h1>
+        <h1>Genero</h1>
+        <h1>Email</h1>
+        <h1>Nacionalidad</h1>
+        <h1>Foto</h1>
+      </div>
     </div>
   );
 }
