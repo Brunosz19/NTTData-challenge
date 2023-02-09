37151e57d3826a21f33dcf201963f65cea97384c
brunosz19 <hesz.bruno05@gmail.com>
2023-02-09 15:08:26 -0500

    UPDATE: replace and put the correct information

--- src/components/rows.js
+++ src/components/rows.js
@@ -3,28 +3,22 @@ import { useAuth } from "../context/auth-context";
 
 export default function useRows() {
   const { dataApi } = useAuth();
-
-  console.log(dataApi);
-
   const dataArray = dataApi?.reduce((acc, curr) => {
     let data = {
       nombre: curr.name.first,
       apellido: curr.name.last,
       edad: curr.dob.age,
-      genero: curr.gender.charAt(0),
+      genero: curr.gender.toUpperCase().charAt(0),
       email: curr.email,
       nacionalidad: curr.nat,
-      foto: curr.picture.large,
-    }
-    console.log(data)
-    acc.push(data)
-    return acc
-  }, []);
+      foto: curr.picture.thumbnail,
+    };
+    
+    acc.push(data);
+    return acc;
+  }, []).sort((a, b) => a.edad - b.edad);
 
-  const rows = useMemo(
-    () => [...dataArray],
-    [dataArray]
-  );
+  const rows = useMemo(() => [...dataArray], [dataArray]);
 
   return rows;
 }
