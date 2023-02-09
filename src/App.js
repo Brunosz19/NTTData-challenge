e3a993e6f02b24484fee23b39342bec473a6e64e
brunosz19 <hesz.bruno05@gmail.com>
2023-02-09 18:03:42 -0500

    REFACTOR: simplify the button for an anchor and the useEffect

--- src/App.js
+++ src/App.js
@@ -13,31 +13,19 @@ function App() {
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
     table;
 
-  let csv = "";
-
   useEffect(() => {
-    let header = columns?.map((col) => col.Header).join(",");
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    csv += header + "\n";
-
-    rows?.forEach((row) => {
-      let dataRow = "";
-      row.cells.forEach((cell) => {
-        dataRow += cell.value + ",";
-      });
-      csv += dataRow + "\n";
-    });
-
-    setCsvData(csv);
-  }, [columns, data]);
+    const header = columns?.map(col => col.Header).join(",");
+    const body = rows?.map(row => row.cells.map(cell => cell.value).join(",")).join("\n");
+    setCsvData(`${header}\n${body}`);
+  }, [columns, data, rows]);
 
   return (
     <div className="tablecontainer">
       <table {...getTableProps()}>
         <thead>
-          {headerGroups.map((headerGroup) => (
+          {headerGroups.map(headerGroup => (
             <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
+              {headerGroup.headers.map(column => (
                 <th
                   {...column.getHeaderProps()}
                   style={{ backgroundColor: "#688dd0", color: "white" }}
@@ -49,50 +37,36 @@ function App() {
           ))}
         </thead>
         <tbody {...getTableBodyProps()}>
-          {rows.map((row) => {
+          {rows.map(row => {
             prepareRow(row);
             return (
               <tr {...row.getRowProps()}>
-                {row.cells.map((cell, index) => {
-                  if (index === row.cells.length - 1) {
-                    return (
-                      <td {...cell.getCellProps()}>
-                        {
-                          <img
-                            style={{ borderRadius: "50%" }}
-                            src={cell.value}
-                            alt="Profile of the person"
-                          />
-                        }
-                      </td>
-                    );
-                  } else {
-                    return (
-                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                    );
-                  }
-                })}
+                {row.cells.map((cell, index) => (
+                  <td {...cell.getCellProps()}>
+                    {index === row.cells.length - 1 ? (
+                      <img
+                        style={{ borderRadius: "50%" }}
+                        src={cell.value}
+                        alt="Profile of the person"
+                      />
+                    ) : (
+                      cell.render("Cell")
+                    )}
+                  </td>
+                ))}
               </tr>
             );
           })}
         </tbody>
       </table>
       {csvData && (
-        <button
+        <a
+          href={`data:text/csv;charset=utf-8,${encodeURIComponent(csvData)}`}
+          download="data.csv"
           className="download-button"
-          onClick={() => {
-            const link = document.createElement("a");
-            link.href = `data:text/csv;charset=utf-8,${encodeURIComponent(
-              csvData
-            )}`;
-            link.download = "data.csv";
-            document.body.appendChild(link);
-            link.click();
-            link.remove();
-          }}
         >
           Download CSV
-        </button>
+        </a>
       )}
     </div>
   );
