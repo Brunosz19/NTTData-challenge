a08d4b8336163032a76698cdb72d4d24ddfc2482
brunosz19 <hesz.bruno05@gmail.com>
2023-02-09 15:07:53 -0500

    ADD: some style to the table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import useColumns from "./components/columns";
 import useRows from "./components/rows";
 import { useTable } from "react-table";
+import "./style/style.css";
 
 function App() {
   const columns = useColumns();
@@ -22,7 +23,7 @@ function App() {
                 // Recorremos cada columna del conjunto para acceder a su información
                 headerGroup.headers.map((column) => (
                   // Añadimos las propiedades a cada celda de la cabecera
-                  <th {...column.getHeaderProps()}>
+                  <th {...column.getHeaderProps()} style={{ backgroundColor: "#688dd0", color: "white"}} >
                     {
                       // Pintamos el título de nuestra columna (propiedad "Header")
                       column.render("Header")
@@ -45,16 +46,29 @@ function App() {
               <tr {...row.getRowProps()}>
                 {
                   // Recorremos cada celda de la fila
-                  row.cells.map((cell) => {
-                    // Añadimos las propiedades a cada celda de la fila
-                    return (
-                      <td {...cell.getCellProps()}>
-                        {
-                          // Pintamos el contenido de la celda
-                          cell.render("Cell")
-                        }
-                      </td>
-                    );
+                  row.cells.map((cell, index) => {
+                    // Verificamos si estamos en la última celda de la fila
+                    if (index === row.cells.length - 1) {
+                      // Añadimos las propiedades a la última celda de la fila
+                      return (
+                        <td {...cell.getCellProps()}>
+                          {
+                            // Pintamos un tag <img> con la data de la celda como "src"
+                            <img style={{borderRadius: "50%"}} src={cell.value} />
+                          }
+                        </td>
+                      );
+                    } else {
+                      // Añadimos las propiedades a las otras celdas de la fila
+                      return (
+                        <td {...cell.getCellProps()}>
+                          {
+                            // Pintamos el contenido de la celda
+                            cell.render("Cell")
+                          }
+                        </td>
+                      );
+                    }
                   })
                 }
               </tr>
